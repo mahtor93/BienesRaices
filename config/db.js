ffefba3928b2746933be17fc7289b8e9c9cd7621
Mario H. Tornería Ramírez <mh.torneria@gmail.com>
2023-07-18 10:49:57 -0400

    ajustes para base de datos

--- config/db.js
+++ config/db.js
@@ -1,11 +1,13 @@
 import { Sequelize } from "sequelize";
+import dotenv from 'dotenv'
+dotenv.config({path:'.env'})
 
 const db = new Sequelize(
-    'bienesraices_node_mvc',
-    'root',
-    '',{
-    host: 'localhost',
-    port: 3306,
+    process.env.BD_NOMBRE,
+    process.env.BD_USER,
+    process.env.BD_PASSWORD,{
+    host: process.env.BD_HOST,
+    port:process.env.BD_PORT,
     dialect: 'mysql',
     define:{
         timestamps:true
