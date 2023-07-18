aeb318156fc7a89e2d029f6dac47dc2a356119d9
Mario H. Tornería Ramírez <mh.torneria@gmail.com>
2023-07-18 11:51:19 -0400

    captura de datos del formulario de registro

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ import db from './config/db.js'
 import usuarioRoutes from './src/routes/usuarioRoutes.js'
 
 const app = express();
+
+//habiltiar lectura de datos de formulario
+app.use( express.urlencoded({extended:true}));
+
+
+//conexión a la base de datos
 try{
     await db.authenticate();
     console.log('Conexión correcta a la base de datos');
@@ -16,7 +22,7 @@ app.listen(port,()=>{
     console.log('Server running at '+port)
 });
 
-//Rutas de archivos
+//Rutas de archivos de vistas
 app.set('view engine', 'pug')
 app.set('views','./views')
 app.use(express.static('public'));
