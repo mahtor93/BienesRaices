881f3192c16db595946dcead49ce4b01b7b42c21
Mario H. Tornería Ramírez <mh.torneria@gmail.com>
2023-07-18 11:23:49 -0400

    programación de la clase Usuario

--- models/Usuario.js
+++ models/Usuario.js
@@ -3,5 +3,30 @@ import db from '../config/db.js'
 
 
 const Usuario = db.define('usuarios',{
-    
+    idUsuario:{
+        type: DataTypes.Integer,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    nombre: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email:{
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    password:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    token:{
+        type: DataTypes.STRING
+    },
+    confirmado:{
+        confirmado:DataTypes.BOOLEAN
+    }
+
 })
+
+export default Usuario;
