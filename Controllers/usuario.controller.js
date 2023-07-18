4f8989e47ead1f0fdb78b9f57fded6f8759411a0
Mario H. Tornería Ramírez <mh.torneria@gmail.com>
2023-07-18 14:41:20 -0400

    programación de las verificaciones del formulario registro

--- Controllers/usuario.controller.js
+++ Controllers/usuario.controller.js
@@ -1,3 +1,4 @@
+import {check,validationResult} from 'express-validator'
 import Usuario from '../models/Usuario.js'
 
 const formularioLogin = (req,res) => {
@@ -9,13 +10,36 @@ const formularioLogin = (req,res) => {
 
 const formularioRegistro = (req,res) => {
     res.render('auth/registro',{
-        tituloPagina:'Crear cuenta'
+        tituloPagina:'Crear Cuenta'
     })
 }
 
 const registrar = async (req,res)=>{
+    try{
+    await check('nombre').notEmpty().withMessage('Nombre obligatorio').run(req);
+
+    await check('email').notEmpty().withMessage('Email obligatorio').run(req);
+    await check('email').isEmail().withMessage('Email invalido').run(req);
+
+    await check('password').notEmpty().withMessage('password obligatorio').run(req);
+    await check('password').isLength({min:6}).withMessage('El password debe ser de al menos 6 caracteres').run(req);
+    await check('repetir_password').equals('password').withMessage('Los passwords no son iguales').run(req);
+
+    let resultado = validationResult(req);
+
+    if(!resultado.isEmpty()){
+        return res.render('auth/registro',{
+            tituloPagina:'Crear Cuenta',
+            errores: resultado.array()
+        })
+    }
+
     const usuario = await Usuario.create(req.body);
-    res.json(usuario)
+    res.json(usuario);
+
+    }catch(error){
+        throw error;
+    }
 
 }
 
@@ -24,10 +48,6 @@ const formularioOlvidePassword = (req,res) => {
         tituloPagina:'Recuperar contraseña'
     })
 }
-
-
-
-
 
 export {
     formularioLogin, formularioRegistro, formularioOlvidePassword, registrar
