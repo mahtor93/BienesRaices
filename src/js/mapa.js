7dfebf8e740a962325a55c508d1b2f2713bc3222
Mario H. Tornería Ramírez <mh.torneria@gmail.com>
2023-07-31 23:21:59 -0400

    implementacion final del mapa en la creación de una nueva entrada de bienes raices

--- src/js/mapa.js
+++ src/js/mapa.js
@@ -1,1 +1,12 @@
-alert('Testeando mapa')
+(function() {
+    const lat = -36.8268850;
+    const lng = -73.0699700;
+    const mapa = L.map('mapa').setView([lat, lng ], 12);
+    
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(mapa);
+
+
+})()
